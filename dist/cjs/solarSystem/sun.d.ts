d94b866e8335e12971107681ac2062e45ef4e1c2
rpawlaszek <rafal.pawlaszek@outlook.com>
2016-04-10 19:53:29 +0200

    Removed dist from repo

--- dist/cjs/solarSystem/sun.d.ts
+++
@@ -1,5 +0,0 @@
-import { EquatorialCoordinates } from '../coordinateSystems/equatorialCoordinates';
-import { UtcDate } from '../datetime/utcdate';
-export declare class Sun {
-    static positionAt(date: UtcDate | Date): EquatorialCoordinates;
-}
